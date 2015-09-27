eb69ffa6eaca49eda277dd70acb32ee13b3edd1e
gatimus <william.g.lohan@gmail.com>
2015-09-27 12:46:00 -0700

    sockets

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/bower_components'));
 
 app.get('/user/:id', function(req, res){
   res.send('user: ' + req.params.id + ', function: ' + req.query.q);
@@ -13,7 +14,21 @@ app.use(function(req, res) {
   res.status(404).send('404');
 });
 
-app.listen(
+
+var users = {};
+io.on('connection',function (socket){
+    users[socket.id] = {name: 'blank'};
+    console.log(users);
+    io.emit('users',users);
+    socket.emit('message',{"alert" : socket.id});
+
+    socket.on('disconnect', function () {
+        users.splice(socket.id,1);
+        console.log(users);
+    });
+});
+
+server.listen(
   app.get('port'),
   function() {
     console.log('Running on port: ', app.get('port'));
